c5d52ff29f6fa71eb29d457db29bd38dc6f2e653
Dreyka <dreyka1989@gmail.com>
2020-06-29 14:46:36 +0300

    Home work

---
+++ ishop/IshopBlock.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let IshopBlock = React.createClass({
+    displayName: "IshopBlock",
+
+    render: function () {
+
+        function createVDOM(v, i, a) {
+            return React.DOM.div({key: v.code, className: "IshopBlockTableRow"},
+                React.DOM.span({className: "Name"}, v.name),
+                React.DOM.span({className: "Price"}, v.price),
+                React.DOM.span({className: "URL_foto"}, v.URL_foto),
+                React.DOM.span({className: "Count"}, v.count),
+            );
+        };
+
+        return React.DOM.div({className: "IshopBlock"},
+            React.DOM.span({className: "IshopBlockName"}, this.props.name),
+            React.DOM.div({className: "IshopBlockTable"}, this.props.products.map(createVDOM)
+            )
+        );
+    },
+});
+
+
